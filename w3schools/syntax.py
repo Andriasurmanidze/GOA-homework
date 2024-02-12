a8feb45591c50b7107229bf8cd9df612e5909c36
Andriasurmanidze <andriasurmanidze51@gmail.com>
2024-02-12 21:45:24 +0400

    commit

---
+++ w3schools/syntax.py
@@ -0,0 +1,11 @@
+x = 10
+a = "nika keshelava is chad"
+
+print(x)
+print(a)
+
+x = "andria"
+y = "andria and rati is old friend"
+
+print(x)
+print(y)
---
+++ w3schools/syntax1.py
@@ -0,0 +1,10 @@
+#print("hello andira surmanidze in goa")
+print("andria is goa's stdent")
+
+a = "nika keshelava is teacher"
+b = "data tezelashvili is asistent"
+c = "goa is bether then novatori"
+
+#პრინტი აღნიშნავს სიტყვას რომელიც კოდმა უნდა გამოიტანოს კონსოლზე მაგალითად
+print(a,b,c)
+#თუ გვინდა რომ ჩვენ ეს სამი ასო რეთად გამოვიტანოდ კონსოლზე მაშინ შემდეგ ნაირად უნდა დავწეროთ print(a,b,c) თუ ჩვენ მძიმეებს არ დავწერთ კოდში ერორი დაფიქსირედება
