3b2ad6a248aece601acef626331a5688369fd7d7
Andriasurmanidze <andriasurmanidze51@gmail.com>
2024-02-16 19:17:33 +0400

    commit

---
+++ w3schools/sayntax eror.py
@@ -0,0 +1,20 @@
+#if 5>2 
+#print("ხუთი მეტია ორზე ")არის შეცდომა შვენ უნდა დავწეროთ შემდეგ ნაირად
+
+if 5 > 2:
+       print("Five is greater than two!")
+
+if 5 > 2:
+print(Five is greater than two!)
+       
+
+#pytonma შეცდომად დაგვიფიქსირა ასე რომდ გავაკეთეთ
+      
+ 
+
+    
+ # ესაა სწორი ფორმა   if 5 > 2:   
+ #         print("Five is greater than two!")
+      
+#ეს კი არასწორი if 5 > 2:
+#print(Five is greater than two!)
