a9653601fc5fd240c5d87629a0fe5a421dcae40c
Andriasurmanidze <andriasurmanidze51@gmail.com>
2024-02-18 02:35:03 +0400

    commit

---
+++ w3schools/nabijiminiliderobisken.py
@@ -0,0 +1,10 @@
+#ჩემი კომპანიის სახელი
+print("GOAL - ORINTED - ACADEMY")
+
+#ეს არის ვითომ კომპანიის სახელი ვითომ
+
+price = "1 moth 310 GEL"
+print(price)
+
+study = "higher academy"
+print(study)
